9342e157b05ff47aa45f680d807990d62b8ba555
wesongaamanda <wesongaamanda@gmail.com>
2026-05-08 11:17:30 +0300

    add

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/folio/',
+  base: '/folio/', 
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
 })
